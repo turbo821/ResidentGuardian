4d6e24d228587364ef487306a9511398a097bc0a
turbo821 <turbo3735@gmail.com>
2025-04-11 02:10:17 +0300

    map implementation

--- frontend/react-client/src/components/MapPage/CityMap.js
+++ frontend/react-client/src/components/MapPage/CityMap.js
@@ -1,9 +1,100 @@
+import { useState, useEffect, useRef } from "react";
+import { YMaps, Map, Placemark, Clusterer, Circle } from "@pbe/react-yandex-maps";
+import "./CityMap.css";
 
-const CityMap = () => {
+const CityMap = ({ issues }) => {
+  const defaultState = {
+    center: [47.517641, 42.160875],
+    zoom: 14,
+  };
+  const [mapCenter, setMapCenter] = useState(defaultState.center);
+  const [hoveredPoint, setHoveredPoint] = useState(null);
+  const mapRef = useRef(null);
+
+  const handleMapClick = async (e) => {
+    const coords = e.get("coords");
+    alert(`${coords[0]} ${coords[1]}`);
+    
+  };
+
+  const handleMouseEnter = (issue) => {
+    if (mapRef.current) {
+      const mapInstance = mapRef.current;
+      try {
+        const globalPixels = mapInstance.options.get("projection").toGlobalPixels(
+          [issue.coords[0], issue.coords[1]],
+          mapInstance.getZoom()
+        );
+        const screenPosition = mapInstance.converter.globalToPage(globalPixels);
+        setHoveredPoint({
+          ...issue,
+          screenPosition,
+        });
+
+      } catch (error) {
+        console.error("Error converting coordinates:", error);
+      }
+    }
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredPoint(null);
+  };
+
+  const handlePlacemarkClick = (issue) => {
+    alert(`${issue.coords[0]}, ${issue.coords[1]}`);
+  };
 
   return (
-    <div className="bg-white p-6 rounded-lg shadow-lg w-full lg:w-3/4 flex items-center justify-center">
-      <span className="text-gray-400 text-lg">[Тут будет карта]</span>
+    <div className="bg-white p-6 rounded-lg shadow-lg w-full lg:w-3/4 flex items-center justify-center h-[750px] map-container">
+      <YMaps query={{ apikey: "ef6ce2bf-6d1d-4567-aaf2-5ca3e0d8da70" }}>
+        <Map
+          defaultState={defaultState}
+          state={{ ...defaultState, center: mapCenter }}
+          width="100%"
+          height="100%"
+          onClick={handleMapClick}
+          options={{
+            suppressMapOpenBlock: true,
+            // restrictMapArea: true // Sets for one city only!
+          }}
+        >
+
+          <Clusterer
+              options={{
+                preset: "islands#invertedVioletClusterIcons",
+                groupByCoordinates: true
+              }}
+            >
+            {issues.map((issue, index) => (
+              <Placemark
+                key={index}
+                geometry={issue.coords}
+                // options={{
+                //   iconColor: issue.color || "#FFFFFF"
+                // }}
+                onClick={() => handlePlacemarkClick(issue)}
+                onMouseEnter={() => handleMouseEnter(issue)}
+                onMouseLeave={handleMouseLeave}
+              />
+            ))}
+          </Clusterer>
+        </Map>
+      </YMaps>
+
+      {hoveredPoint && hoveredPoint.screenPosition && (
+        <div
+          className="hovered-point-window"
+          style={{
+            top: `${hoveredPoint.screenPosition[1] - 230}px`,
+            left: `${hoveredPoint.screenPosition[0] + 20}px`,
+          }}
+        >
+          <p><strong>{hoveredPoint.title}</strong></p>
+          <p>{hoveredPoint?.status}</p>
+          <p>Местонахождение: {`${hoveredPoint.coords[0]}, ${hoveredPoint.coords[1]}`}</p>
+        </div>
+      )}
     </div>
   )
 }
--- frontend/react-client/src/components/MapPage/MapFilters.js
+++ frontend/react-client/src/components/MapPage/MapFilters.js
@@ -6,42 +6,42 @@ const MapFilters = () => {
   const [date, setDate] = useState("");
 
   return (
-    <div className="bg-white p-6 rounded-lg shadow-lg w-full lg:w-1/4 mb-6 lg:mb-0">
-    <h2 className="text-2xl font-bold text-gray-800 mb-4">Фильтр</h2>
+    <div className="bg-white p-6 rounded-lg shadow-lg w-full lg:w-1/4 mb-6 lg:mb-0 mx-3">
+      <h2 className="text-2xl font-bold text-gray-800 mb-4">Фильтр</h2>
 
-    <label className="block text-gray-700 font-medium">Категория:</label>
-    <select 
-      value={category} 
-      onChange={(e) => setCategory(e.target.value)} 
-      className="w-full p-2 border rounded-lg mb-4"
-    >
-      <option value="all">Все</option>
-      <option value="road">Дорожные проблемы</option>
-      <option value="lighting">Освещение</option>
-      <option value="garbage">Мусор</option>
-      <option value="other">Другое</option>
-    </select>
+      <label className="block text-gray-700 font-medium">Категория:</label>
+      <select 
+        value={category} 
+        onChange={(e) => setCategory(e.target.value)} 
+        className="w-full p-2 border rounded-lg mb-4"
+      >
+        <option value="all">Все</option>
+        <option value="road">Дорожные проблемы</option>
+        <option value="lighting">Освещение</option>
+        <option value="garbage">Мусор</option>
+        <option value="other">Другое</option>
+      </select>
 
-    <label className="block text-gray-700 font-medium">Статус:</label>
-    <select 
-      value={status} 
-      onChange={(e) => setStatus(e.target.value)} 
-      className="w-full p-2 border rounded-lg mb-4"
-    >
-      <option value="all">Все</option>
-      <option value="pending">В ожидании</option>
-      <option value="in-progress">В процессе</option>
-      <option value="resolved">Решенные</option>
-    </select>
+      <label className="block text-gray-700 font-medium">Статус:</label>
+      <select 
+        value={status} 
+        onChange={(e) => setStatus(e.target.value)} 
+        className="w-full p-2 border rounded-lg mb-4"
+      >
+        <option value="all">Все</option>
+        <option value="pending">В ожидании</option>
+        <option value="in-progress">В процессе</option>
+        <option value="resolved">Решенные</option>
+      </select>
 
-    <label className="block text-gray-700 font-medium">Дата:</label>
-    <input 
-      type="date" 
-      value={date} 
-      onChange={(e) => setDate(e.target.value)} 
-      className="w-full p-2 border rounded-lg"
-    />
-  </div>
+      <label className="block text-gray-700 font-medium">Дата:</label>
+      <input 
+        type="date" 
+        value={date} 
+        onChange={(e) => setDate(e.target.value)} 
+        className="w-full p-2 border rounded-lg"
+      />
+    </div>
   );
 }
 
